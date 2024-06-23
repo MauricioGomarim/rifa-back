6aa63578cdc67121008dd62b701e4072cbbcfab5
Mauricio Gomarim <mauricio.gomarim3@gmail.com>
2024-06-23 18:36:05 -0300

    te

--- src/controllers/RifasController.js
+++ src/controllers/RifasController.js
@@ -111,8 +111,8 @@ class RifasController {
 
         if (newNumbers.length === 0) {
         } else {
-          const insertData = newNumbers.map((num) => ({ numero: num }));
-          await knex("cotas_rifas").insert({insertData, cpf});
+          const insertData = newNumbers.map((num) => ({ numero: num, cpf }));
+          await knex("cotas_rifas").insert({insertData});
         }
       } catch (error) {
         console.error("Erro ao cadastrar números:", error);
