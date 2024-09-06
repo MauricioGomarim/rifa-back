134d66c302225b99e34300dfdd64a96cd7eda66f
Mauricio Gomarim <mauricio.gomarim3@gmail.com>
2024-09-06 01:08:26 -0300

    teste

--- migrations/20240602201523_cotas_rifas.js
+++ migrations/20240602201523_cotas_rifas.js
@@ -2,7 +2,6 @@ exports.up = (knex) =>
     knex.schema.createTable("cotas_rifas", (table) => {
       table.increments("id");
       table.text("numero");
-      table.text("nome");
       table.text("cpf");
       table.timestamp("created_at").default(knex.fn.now());
       table.timestamp("updated_at").default(knex.fn.now());
